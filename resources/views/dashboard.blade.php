02eb918f9d527c5f9f939bee152d744f8d583df1
Usudesu <gsandovaliq@gmail.com>
2025-12-16 00:41:43 -0600

    feat(dashboard) Cambio contenido al dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,9 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
+
+        
+
     </x-slot>
 
     <div class="py-12">
@@ -14,4 +17,18 @@
             </div>
         </div>
     </div>
+
+    <div class="relative w-full text-center">
+            <div class="flex max-w-[335px] mx-auto dark:text-[#EDEDEC] border-[#19140035]">
+                Bienvenido
+            </div>
+
+            <p class="max-w-7xl mx-auto sm:px-6 lg:px-8 dark:text-gray-200">
+                {{ __("Este es tu dashboard")}}
+            </p>
+
+    </div>
+
+
 </x-app-layout>
+
